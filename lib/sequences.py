7cf315280a5e8d9f82267543976f060b18599edf
Eric <ericmuthuipatch22@gmail.com>
2023-08-18 16:27:50 +0300

    Completed assignment

--- lib/sequences.py
+++ lib/sequences.py
@@ -1,4 +1,13 @@
 #!/usr/bin/env python3
 
 def print_fibonacci(length):
-    pass
+    fibonacci_list = []
+    a, b = 0, 1
+    for i in range(length):
+        fibonacci_list.append(a)
+        a, b=b, a+b
+    print(fibonacci_list)
+
+
+
+
